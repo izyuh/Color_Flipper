f875163b1dfb062b4acb0ca25188668056fb232b
isaias <isaias.gutierrez48@gmail.com>
2024-07-23 14:38:18 -0700

    added more color options by using Math function

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ button.addEventListener('click', changeColor)
 
 function changeColor(){
     const body = document.body
-    const colors = ['#20405c','#5cc941','#c941c2','#da7421','#ccec16','#e62121']
 
-    const random = Math.floor(Math.random() *colors.length)
-    const color = colors[random]
+    const red = Math.floor(Math.random() * 255)
+    const green = Math.floor(Math.random() * 255)
+    const blue = Math.floor(Math.random() * 255)
+
+    const color = `rgb(${red},${green},${blue})`
+    
     displayColor.textContent = color
     body.style.background = color
 }
